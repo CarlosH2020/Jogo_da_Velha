9e0876cfcbdc1d7c3af5aba22752bcd032238395
Root <Root@DESKTOP-QDNN9NT>
2020-10-26 22:02:47 -0300

    clculadora22_09_2020

---
+++ src/jogo_da_velha/Computador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogo_da_velha;
+
+/**
+ *
+ * @author Root
+ */
+public class Computador extends Jogador{
+    
+    public Computador(int jogador){
+        super(jogador);
+        System.out.println("Jogador 'Computador' criado!");
+    }
+    
+    
+    public void jogar(Tabuleiro tabuleiro){
+        
+    }
+    
+   
+    public void Tentativa(Tabuleiro tabuleiro){
+        
+    }
+}
